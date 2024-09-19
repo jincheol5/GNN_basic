f3a1f2810984f028de1c8984630ab6958aea7d46
jincheol <oscar4293@gmail.com>
2024-09-19 23:23:51 +0900

    update

--- GNN/app_train.py
+++ GNN/app_train.py
@@ -1,8 +1,3 @@
-import sys
-import os
-# 현재 파일의 상위 디렉토리(GNN) 경로를 Python 경로에 추가
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import torch
 from data_load import DataLoader
 from model import R_GNN
