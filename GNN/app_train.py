88aa02a6e715918c04bce1eb5f3aaa83bcfb757c
jincheol <oscar4293@gmail.com>
2024-09-21 17:36:33 +0900

    update

--- GNN/app_train.py
+++ GNN/app_train.py
@@ -6,17 +6,17 @@ from data_load import DataLoad
 from model import R_GNN
 from model_train import Trainer
 
-seed= 42
-random.seed(seed) # Python의 기본 random seed 설정
-np.random.seed(seed) # NumPy의 random seed 설정
-torch.manual_seed(seed) # PyTorch의 random seed 설정
-os.environ["PYTHONHASHSEED"] = str(seed)
-# CUDA 사용 시, 모든 GPU에서 동일한 seed 사용
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-# PyTorch의 난수 생성 결정론적 동작 보장 (동일 연산 결과 보장)
-torch.backends.cudnn.deterministic = True 
-torch.backends.cudnn.benchmark = False
+# seed= 42
+# random.seed(seed) # Python의 기본 random seed 설정
+# np.random.seed(seed) # NumPy의 random seed 설정
+# torch.manual_seed(seed) # PyTorch의 random seed 설정
+# os.environ["PYTHONHASHSEED"] = str(seed)
+# # CUDA 사용 시, 모든 GPU에서 동일한 seed 사용
+# torch.cuda.manual_seed(seed)
+# torch.cuda.manual_seed_all(seed)
+# # PyTorch의 난수 생성 결정론적 동작 보장 (동일 연산 결과 보장)
+# torch.backends.cudnn.deterministic = True 
+# torch.backends.cudnn.benchmark = False
 
 
 
